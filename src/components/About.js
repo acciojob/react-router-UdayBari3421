bd9f682887bf6b88431daa6a68f555143760268c
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-29 14:52:38 +0000

    msg

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        This is a simple application to demonstrate the usage of react-router.
+      </p>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return <h1>Welcome to my Website!</h1>;
+};
+
+export default Home;
